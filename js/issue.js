ebf7182dfb27f3cea4cafd59ce68bb2cf59aa5fe
Jan van Mulligen <jcvanmulligen@gmail.com>
2018-05-03 13:35:29 +0200

    update

--- js/issue.js
+++ js/issue.js
@@ -1,25 +1,26 @@
 
 function storeIssuer() {
 
-  let issuerType                  = document.getElementById("issuerType").value;
-  let issuerId                    = document.getElementById("issuerId").value;
-  let issuerName                  = document.getElementById("issuerName").value;
-  let issuerUrl                   = document.getElementById("issuerUrl").value;
-  let issuerEmail                 = document.getElementById("issuerEmail").value;
-  let issuerSmartcontractAddress  = document.getElementById("issuerSmartcontractAddress").value;
-  let issuerImage                 = document.getElementById("issuerImage").value;
-  let issuerImageUrl              = document.getElementById("issuerImageUrl").value;
+  let issuerType = document.getElementById("issuerType").value;
+  let issuerId = document.getElementById("issuerId").value;
+  let issuerName = document.getElementById("issuerName").value;
+  let issuerUrl = document.getElementById("issuerUrl").value;
+  let issuerEmail = document.getElementById("issuerEmail").value;
+  let issuerSmartcontractAddress = document.getElementById("issuerSmartcontractAddress").value;
+  let issuerImage = document.getElementById("issuerImage").value;
+  let issuerImageUrl = document.getElementById("issuerImageUrl").value;
 
 
-  let issuerdata = ["issuerType:"     + issuerType, 
-                    "issuerId:"       + issuerId, 
-                    "issuerName:"     + issuerName,
-                    "issuerUrl:"      + issuerUrl, 
-                    "issuerEmail:"    + issuerUrl, 
-                    "issuerSmartcontractAddress:" + issuerSmartcontractAddress,
-                    "issuerImage:"    + issuerImage, 
-                    "issuerImageUrl:" + issuerImageUrl
-                  ];
+  let issuerdata = [
+    "issuerType:" + issuerType,
+    "issuerId:" + issuerId,
+    "issuerName:" + issuerName,
+    "issuerUrl:" + issuerUrl,
+    "issuerEmail:" + issuerUrl,
+    "issuerSmartcontractAddress:" + issuerSmartcontractAddress,
+    "issuerImage:" + issuerImage,
+    "issuerImageUrl:" + issuerImageUrl
+  ];
 
 
 
@@ -32,7 +33,7 @@ function storeIssuer() {
     issuerdata = JSON.parse(storedData);
 
 
-}
+  }
 
 }
 
@@ -40,22 +41,24 @@ function storeIssuer() {
 
 function storeBadge() {
 
-  let badgeType         = document.getElementById("badgeType").value;
-  let badgeId           = document.getElementById("badgeId").value;
-  let badgeName         = document.getElementById("badgeName").value;
-  let badgeDescription  = document.getElementById("badgeDescription").value;
-  let badgeCriteria     = document.getElementById("badgeCriteria").value;
-  let badgeImage        = document.getElementById("badgeImage").value;
+  let badgeType = document.getElementById("badgeType").value;
+  let badgeId = document.getElementById("badgeId").value;
+  let badgeName = document.getElementById("badgeName").value;
+  let badgeDescription = document.getElementById("badgeDescription").value;
+  let badgeCriteria = document.getElementById("badgeCriteria").value;
+  let badgeImage = document.getElementById("badgeImage").value;
+  let badgeImageUrl = document.getElementById("badgeImageUrl").value;
 
 
   let badgedata = [
-                  "badgeType:" + badgeType,
-                  "badgeId:" + badgeId,
-                  "badgeName:" + badgeName,
-                  "badgeDescription:" + badgeDescription,
-                  "badgeCriteria:" + badgeCriteria, 
-                  "badgeImage:" + badgeImage,
-                  ];
+    "badgeType:" + badgeType,
+    "badgeId:" + badgeId,
+    "badgeName:" + badgeName,
+    "badgeDescription:" + badgeDescription,
+    "badgeCriteria:" + badgeCriteria,
+    "badgeImage:" + badgeImage,
+    "badgeImageUrl:" + badgeImageUrl
+  ];
 
 
 
@@ -68,6 +71,22 @@ function storeBadge() {
     badgedata = JSON.parse(storedData);
 
 
+  }
+
+  
+  function storeRecipient() {
+    let recipientType = document.getElementById("recipienttype").value
+
+    let recipientdata = [
+      "recipienttype:" + recipientType,
+    ]
+
+    sessionStorage.setItem("recipientData", JSON.stringify(recipientdata));
+
+    let storeData = sessionStorage.getItem("recipientdata");
+    if (storedData) {
+      recipientdata = JSON.parse(storedData);
+    }
   }
 
 }
