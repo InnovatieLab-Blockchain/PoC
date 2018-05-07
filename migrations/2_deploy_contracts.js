bde3b12e69e56733492f665b811470321dbaafb2
tapdennis <dennis.tap@duo.nl>
2018-05-07 12:50:22 +0200

    Voor fase 1: Smart contract voor een Issuer met de relevante gegevens. Interface met operaties die een issuer kan uitvoeren. Smart contract voor bijv. de RuG, inclusief implementaties voor de operaties.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-var issuer = artifacts.require("./Issuer.sol");
+var issuer = artifacts.require("./IssuerRuG.sol");
 
 module.exports = function (deployer) {
     deployer.deploy(issuer, '2ojKNym3x16kUNQPq32CNcRpuo8MDH5w5vQ', 'Issuer', 'RuG', 'http://www.rug.nl', 'info@rug.nl');
