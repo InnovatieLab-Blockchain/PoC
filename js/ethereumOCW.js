cf1b3c5aee42d5c36d511299a1e31409ee215129
Jan van Mulligen <jcvanmulligen@gmail.com>
2018-05-25 10:22:56 +0200

    fdafd

--- js/ethereumOCW.js
+++ js/ethereumOCW.js
@@ -8,22 +8,19 @@ if (typeof web3 !== 'undefined') {
 web3 = new Web3(web3Provider);
 
 let abi = [{ "constant": false, "inputs": [{ "name": "data", "type": "string" }], "name": "storeStatistics", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "issuer", "type": "address" }], "name": "getStatisticsFor", "outputs": [{ "name": "", "type": "string[]" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "", "type": "string" }, { "indexed": false, "name": "", "type": "address" }, { "indexed": false, "name": "", "type": "string" }], "name": "LogStoreStatistics", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "", "type": "string" }, { "indexed": false, "name": "", "type": "address" }], "name": "LogGetStatistics", "type": "event" }];
-let contractAddress = "0xA68E8aB9Ae51beCE48cA73d15a9b9fBEA1253937";
+let contractAddress = "0x350BdAfafD67309687946Ff910eb5a6064d96C05";
 let contract = web3.eth.contract(abi).at(contractAddress);
 
 function getStatisticalData() {
-    //TODO: extract json from uploaded file and hash
-    // let hash = Sha256.hash(getJsonFromUrl(document.getElementById("revokeHash").value));
-
-    let address_issuer = "0x350BdAfafD67309687946Ff910eb5a6064d96C05";
+    
+    let address_issuer = "0x76ee3fDE66d39797BDfca0bB002b40d2B3EbFCa9";
     contract.getStatisticsFor.call(address_issuer, {
         from: web3.eth.accounts[0],
         gas: 4000000
     },
         function (error, result) {
             if (!error) {
-                console.log(result);
-                
+                console.log(result);                
                 sessionStorage.setItem("createdMetaData", JSON.stringify(result));
                 displayContractMetaData();
             } else {
--- js/uport.js
+++ js/uport.js
@@ -14,7 +14,8 @@ const universiteitCredentials = new Credentials({
     address: '2ojKNym3x16kUNQPq32CNcRpuo8MDH5w5vQ',
     network: 'rinkeby',
     signer: SimpleSigner('11cba7f19bce365f70b85e029f048f5c4cc73ad3232db828af5107dbb865e2b6')
-});
+}); 
+
 
 
 
