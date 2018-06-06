8d0d2e0cd340bdac43613ef1193e8db4f8d10811
Jan van Mulligen <jcvanmulligen@gmail.com>
2018-06-06 14:34:35 +0200

    commit

--- js/attest.js
+++ js/attest.js
@@ -3,17 +3,27 @@
 const addEntry = () => {
     let e = document.getElementById('diploma');
     let studienaam = e.options[e.selectedIndex].text;
+    alert(studienaam);
     let salt = 'groningen';
+    let jsonObj = "test";
+    
+    if (localStorage.getItem(studienaam) === undefined) {
+        localStorage.setItem(studienaam, JSON.stringify(""))
+    
+    } else {    
+    let jsonObj = JSON.parse(localStorage.getItem(studienaam))
+    }
 
-    let jsonObj = JSON.parse(localStorage.getItem(studienaam));
+    alert(jsonObj);
     let newId = document.getElementById("entryId").value;
     let newValue = document.getElementById("entryId").value;
-    jsonObj.studie.studenten[newId] = newValue;
+    jsonObj[newId] = newValue;
 
     console.log(jsonObj)
     localStorage.setItem(studienaam, JSON.stringify(jsonObj))
     alert('id: ' + newId + '\n' + 'salt: ' + newValue + '\n' + 'added to: ' + studienaam);
-};
+    }    
+    ;
 
 
 
